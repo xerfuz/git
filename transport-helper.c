b3e15e52e03fef8f619ba1a809373392338542d5
Brandon Williams <bmwill@google.com>
2018-01-25 15:58:35 -0800

    transport-helper: introduce stateless-connect
    Introduce the transport-helper capability 'stateless-connect'.  This
    capability indicates that the transport-helper can be requested to run
    the 'stateless-connect' command which should attempt to make a
    stateless connection with a remote end.  Once established, the
    connection can be used by the git client to communicate with
    the remote end natively in a stateless-rpc manner as supported by
    protocol v2.  This means that the client must send everything the server
    needs in a single request as the client must not assume any
    state-storing on the part of the server or transport.
    
    If a stateless connection cannot be established then the remote-helper
    will respond in the same manner as the 'connect' command indicating that
    the client should fallback to using the dumb remote-helper commands.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport-helper.c
+++ transport-helper.c
@@ -26,6 +26,7 @@ struct helper_data {
 		option : 1,
 		push : 1,
 		connect : 1,
+		stateless_connect : 1,
 		signed_tags : 1,
 		check_connectivity : 1,
 		no_disconnect_req : 1,
@@ -188,6 +189,8 @@ static struct child_process *get_helper(struct transport *transport)
 			refspecs[refspec_nr++] = xstrdup(arg);
 		} else if (!strcmp(capname, "connect")) {
 			data->connect = 1;
+		} else if (!strcmp(capname, "stateless-connect")) {
+			data->stateless_connect = 1;
 		} else if (!strcmp(capname, "signed-tags")) {
 			data->signed_tags = 1;
 		} else if (skip_prefix(capname, "export-marks ", &arg)) {
@@ -612,6 +615,11 @@ static int process_connect_service(struct transport *transport,
 	if (data->connect) {
 		strbuf_addf(&cmdbuf, "connect %s\n", name);
 		ret = run_connect(transport, &cmdbuf);
+	} else if (data->stateless_connect) {
+		strbuf_addf(&cmdbuf, "stateless-connect %s\n", name);
+		ret = run_connect(transport, &cmdbuf);
+		if (ret)
+			transport->stateless_rpc = 1;
 	}
 
 	strbuf_release(&cmdbuf);
